ae22ff769ab0e52b60966d82fd18e4a7f71891b7
Nero Sule <sulenero@gmail.com>
2016-06-14 16:35:30 +0100

    Adds extension methods for display templates

---
+++ src/ProjectSampleCore/Infrastructure/Mvc/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace ProjectSampleCore.Infrastructure.Mvc.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static async Task<IHtmlContent> DisplayTemplateFor<T>(this IHtmlHelper html, T item)
+            => await html.PartialAsync($"DisplayTemplates/{item.GetType().Name}", item);
+
+        public static async Task<IHtmlContent> EditorTemplateFor<T>(this IHtmlHelper html, T item)
+            => await html.PartialAsync($"EditorTemplates/{item.GetType().Name}", item);
+    }
+}
